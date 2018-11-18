047c5f1f07fb076b4cd447ddb9a8ea355bf3750a
Thomas de Beauchêne <thomas.de.beauchene@sap.com>
2018-11-18 17:28:19 +0100

    synchronous compilation to avoid (require 'vterm-module) error

--- vterm.el
+++ vterm.el
@@ -7,25 +7,26 @@
 
 ;;; Code:
 
+;;;###autoload
 (defun vterm-module-compile ()
   "This function compiles the vterm-module."
   (interactive)
-  (let ((buffer (get-buffer-create " *Install vterm"))
-        (default-directory (file-name-directory (locate-library "vterm"))))
-    (make-process
-     :name "build-vterm-module"
-     :buffer buffer
-     :command '("sh" "-c" "mkdir -p build; cd build; cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..; make")
-     :stderr buffer
-     :sentinel (lambda (process status)
-                 (if (equal status "finished\n")
-                     (progn (let ((buffer (process-buffer process)))
-                              (bury-buffer buffer)
-                              (when-let ((window (get-buffer-window buffer)))
-                                (delete-window window)))
-                            (message "Sucessfully compiled the emacs-libvterm module."))
-                   (message "Compilation of libvterm has failed."))))
-    (pop-to-buffer buffer)))
+  (let ((default-directory (file-name-directory (locate-library "vterm"))))
+    (unless (file-executable-p (concat default-directory "vterm-module.so" ))
+      (let ((buffer (get-buffer-create " *Install vterm")))
+        (pop-to-buffer buffer)
+        (let ((status (call-process "sh" nil buffer t "-c"
+                                    "mkdir -p build;                             \
+                                     cd build;                                   \
+                                     cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..; \
+                                     make")))
+          (if (eq status 0)
+              (progn
+                (bury-buffer buffer)
+                (when-let ((window (get-buffer-window buffer)))
+                  (delete-window window))
+                (message "Compilation of emacs-libvterm module succeeded"))
+            (message "Compilation of emacs-libvterm module failed")))))))
 
 (when (boundp 'vterm-install)
   (vterm-module-compile))
