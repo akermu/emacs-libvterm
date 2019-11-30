28c090d08500ff5bb334a8fc9931844b225bab6e
Yuan Fu <casouri@gmail.com>
2019-11-29 19:59:27 -0500

    Add some convenience functions
    Add vterm--rename-buffer-as-title and vterm-toggle
    
    * vterm.el (vterm--rename-buffer-as-title, vterm--toggle-prev-buffer,
    vterm--buffer-list, vterm--cd, vterm-toggle): add

--- vterm.el
+++ vterm.el
@@ -722,5 +722,75 @@ the called functions."
         (apply (cadr f) args)
       (message "Failed to find command: %s" command))))
 
+;;; Convenience
+
+(defun vterm--rename-buffer-as-title (title)
+  "Rename buffer name as the xterm TITLE."
+  (rename-buffer (format "vterm @ %s" title) t))
+
+(defvar vterm--toggle-prev-buffer nil
+  "Used by ‘vterm-toggle’.")
+
+(defun vterm--buffer-list ()
+  "Return a list of vterm buffers."
+  (cl-remove-if-not (lambda (buf)
+                      (provided-mode-derived-p
+                       (buffer-local-value 'major-mode buf)
+                       'vterm-mode))
+                    (buffer-list)))
+
+(defun vterm--cd (dir)
+  "Cd to DIR."
+  (when (derived-mode-p 'vterm-mode)
+    (vterm-send-key "u" nil nil t)
+    (execute-kbd-macro (format "cd %s" dir))))
+
+(defun vterm-toggle (&optional arg)
+  "Toggle vterm.
+
+ARG: C-u: open the vterm buffer with the same dir of current buffer
+If there exists an Vterm buffer with current directory, use that,
+otherwise create one.
+
+C-u C-u: same as C-u, but reuse a existing vterm buffer instead of
+creating one."
+  (interactive "p")
+  (if (derived-mode-p 'vterm-mode)
+      ;; toggle off
+      (if vterm--toggle-prev-buffer
+          (switch-to-buffer vterm--toggle-prev-buffer)
+        (user-error "No previous buffer to jump back to"))
+    ;; toggle on
+    (let ((buffer-list (vterm--buffer-list)))
+      (cond
+       ((or (eq arg 4) ; C-u
+            (eq arg 16)) ; C-u C-u
+        ;; open in current dir
+        (let* ((dir default-directory)
+               (buffer-with-same-dir
+                (catch 'found
+                  (dolist (buffer buffer-list nil)
+                    (when (equal dir (buffer-local-value
+                                      'default-directory
+                                      buffer))
+                      (throw 'found buffer))))))
+          ;; found the buffer with the same dir
+          (setq vterm--toggle-prev-buffer
+                (current-buffer))
+          (if buffer-with-same-dir
+              (switch-to-buffer buffer-with-same-dir)
+            ;; or not and maybe create a new one
+            (switch-to-buffer
+             (if (eq arg 16)
+                 (progn (message "No valid vterm buffer found, reuse one.")
+                        (car buffer-list))
+               (message "No valid vterm buffer found, create a new one.")
+               (vterm)))
+            (vterm--cd dir))))
+       ;; simply open
+       (t (setq vterm--toggle-prev-buffer (current-buffer))
+          (switch-to-buffer (or (car buffer-list)
+                                (vterm))))))))
+
 (provide 'vterm)
 ;;; vterm.el ends here
