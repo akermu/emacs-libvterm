a746e58bdd8d0497ce767f54c102aa14c37cb97f
Gabriele Bozzola <sbozzolator@gmail.com>
2020-02-15 09:46:28 -0700

    Add vterm-compile-with-system-libvterm variable

--- vterm-module-make.el
+++ vterm-module-make.el
@@ -5,19 +5,30 @@
 (defvar vterm-install-buffer-name " *Install vterm"
   "Name of the buffer used for compiling vterm-module.")
 
+(defcustom vterm-compile-with-system-libvterm nil
+  "Compile vterm with the system libvterm instead of downloading
+and a local copy from GitHub."
+  :type  'boolean
+  :group 'vterm)
+
 ;;;###autoload
 (defun vterm-module-compile ()
-  "This function compiles the vterm-module."
+  "This function compiles the vterm-module.
+
+If the variable vterm-compile-with-system-libvterm is set to t,
+the system libvterm is used."
   (interactive)
   (let* ((default-directory
            (file-name-directory (file-truename (locate-library "vterm"))))
          ;; Ask the user if they want to compile with the version of libvterm
          ;; on the system. It has to be available! (We don't perform checks)
          (compile-with-system-libvterm
-          (if (y-or-n-p
-                   "Do you want to use the system libvterm? (It has to be installed)")
+          (if (or
+               vterm-compile-with-system-libvterm
+               (y-or-n-p
+                "Do you want to use the system libvterm? (It has to be installed separately)"))
               "yes"
-              "no"))
+            "no"))
          (make-commands
           (concat
            "mkdir -p build;"
