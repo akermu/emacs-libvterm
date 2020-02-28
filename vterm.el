f70895cf016359bb22a72f3f85cbee9ad85f9c19
jixiuf <jixiuf@qq.com>
2020-02-28 11:36:54 +0800

    vterm-copy-mode-map bind vterm-beginning-of-line.

--- vterm.el
+++ vterm.el
@@ -406,6 +406,7 @@ This is the value of `next-error-function' in Compilation buffers."
 (define-key vterm-copy-mode-map [return]               #'vterm-copy-mode-done)
 (define-key vterm-copy-mode-map (kbd "RET")            #'vterm-copy-mode-done)
 (define-key vterm-copy-mode-map (kbd "C-c C-r")        #'vterm-reset-cursor-point)
+(define-key vterm-copy-mode-map (kbd "C-a")            #'vterm-beginning-of-line)
 
 
 (define-minor-mode vterm-copy-mode
@@ -848,6 +849,21 @@ the called functions."
          (or (= pt prompt-pt)
              (string-blank-p (buffer-substring-no-properties pt prompt-pt))))))
 
+(defun vterm-beginning-of-line ()
+"Move point to the beginning of the line.
+
+Move the point to the first character after the shell prompt on this line.
+If the point is already there, move to the beginning of the line.
+Effectively toggle between the two positions."
+  (interactive)
+  (let ((pt (point))
+        (prompt-pt (vterm--get-prompt-point)))
+    (if (= pt prompt-pt)
+        (beginning-of-line)
+      (if (= (line-number-at-pos prompt-pt)
+             (line-number-at-pos pt))
+          (goto-char prompt-pt)
+        (beginning-of-line)))))
 
 (provide 'vterm)
 ;;; vterm.el ends here
