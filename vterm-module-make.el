e3183af1a1a0711402b419321e4e585c2464935d
Gabriele Bozzola <sbozzolator@gmail.com>
2020-03-01 16:52:15 -0700

    Add vterm-module--cmake-is-available

--- vterm-module-make.el
+++ vterm-module-make.el
@@ -5,10 +5,20 @@
 (defvar vterm-install-buffer-name " *Install vterm"
   "Name of the buffer used for compiling vterm-module.")
 
+(defun vterm-module--cmake-is-available ()
+  "Return t if cmake is available.
+CMake is needed to build vterm, here we check that we can find
+the executable."
+
+  (unless (executable-find "cmake")
+    (error "Vterm needs CMake to be compiled. Please, install CMake"))
+  t)
+
 ;;;###autoload
 (defun vterm-module-compile ()
   "This function compiles the vterm-module."
   (interactive)
+  (when (vterm-module--cmake-is-available)
   (let ((default-directory
           (file-name-directory (file-truename (locate-library "vterm"))))
         (make-commands
@@ -23,7 +33,7 @@
         (pop-to-buffer vterm-install-buffer-name)
         (if (zerop (call-process "sh" nil buffer t "-c" make-commands))
             (message "Compilation of emacs-libvterm module succeeded")
-          (error "Compilation of emacs-libvterm module failed!"))))))
+          (error "Compilation of emacs-libvterm module failed!")))))))
 
 (or (require 'vterm-module nil t)
     (vterm-module-compile))
