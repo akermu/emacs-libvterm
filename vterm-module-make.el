abf1e66ee0bdfeaa70ba86f9889c3d2371c9b751
Gabriele Bozzola <sbozzolator@gmail.com>
2020-03-01 16:33:17 -0700

    Add vterm-module-cmake-is-available
    CMake is needed to build vterm. We have to make sure that it is
    available.

--- vterm-module-make.el
+++ vterm-module-make.el
@@ -5,45 +5,14 @@
 (defvar vterm-install-buffer-name " *Install vterm"
   "Name of the buffer used for compiling vterm-module.")
 
-(defun vterm-module-compilation-requirements-satisfied ()
-  "Return t if the requirements for the successful compilation are satisfied.
+(defun vterm-module-cmake-is-available ()
+  "Return t if cmake is available.
 
-This means checking if cmake is available, if it is newer than
-version 3.11.0, and if libtool is available."
+CMake is needed to build vterm, here we check that we can find
+the executable."
 
-  ;; Check if cmake is available
   (unless (executable-find "cmake")
-    (error "Vterm needs to be compiled but a dependency, cmake, is missing"))
-
-  ;; Now we can assume cmake is available, but is it new enough?
-  ;; We need a minimum version, at the moment 3.11.0, we check this
-  ;; by invoking cmake --version and matching the number inside
-  ;; At the moment, the output of this command looks like
-  ;;
-  ;; cmake version 3.14.6
-  ;;
-  ;; CMake suite maintained and supported by Kitware (kitware.com/cmake).
-  ;;
-  ;; So, we extract the version from the first line.
-  (let* ((cmake-min-required-version "3.11.0")
-        (cmake-version-output (shell-command-to-string "cmake --version"))
-        (cmake-version
-         (progn
-           (string-match "^cmake version \\([0-9.]+\\)" cmake-version-output)
-           (match-string 1 cmake-version-output))))
-    (when
-        (string< cmake-version cmake-min-required-version)
-    (error (format
-            "Your current version of cmake (%s) is too old for vterm (required: %s)"
-            cmake-version
-            cmake-min-required-version))))
-
-  ;; Check if libtool (on macOS glibtool) is available
-  (unless (or (executable-find "libtool") (executable-find "glibtool"))
-    (error "Vterm needs to be compiled but a dependency, libtool, is missing"))
-
-  ;; If we are here it means that all the dependencies are satisfied.
-  ;; We can return t, anything else should have thrown an error.
+      (error "Vterm needs to be compiled but a dependency, cmake, is missing"))
   t)
 
 
@@ -51,7 +20,7 @@ version 3.11.0, and if libtool is available."
 (defun vterm-module-compile ()
   "This function compiles the vterm-module."
   (interactive)
-  (when (vterm-module-compilation-requirements-satisfied)
+  (when (vterm-module-cmake-is-available)
       (let ((default-directory
           (file-name-directory (file-truename (locate-library "vterm"))))
         (make-commands
