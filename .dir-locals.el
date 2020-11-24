101ff86d4d1c7ff8ca954ba59f666fd36f789aae
jixiuf <jixiuf@qq.com>
2020-11-24 17:51:30 +0800

    remove .dir-locals.el  as suggested by #441

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((projectile-project-compilation-cmd . "mkdir -p build; cd build; cmake ..; make")
-         (projectile-project-run-cmd . "mkdir -p build; cd build; cmake ..; make run")
-         )))
