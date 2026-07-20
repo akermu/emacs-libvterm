70921114908ebb260d6686db8cbe2445a64f90a2
George Huebner <ghuebner@anduril.com>
2026-07-20 13:28:16 -0700

    fix: avoid parsing user input as options in emacs-vterm.fish

--- etc/emacs-vterm.fish
+++ etc/emacs-vterm.fish
@@ -40,7 +40,7 @@ end
 function vterm_cmd --description 'Run an Emacs command among the ones defined in vterm-eval-cmds.'
     set -l vterm_elisp ()
     for arg in $argv
-        set -a vterm_elisp (printf '"%s" ' (string replace -a -r '([\\\\"])' '\\\\\\\\$1' $arg))
+        set -a vterm_elisp (printf '"%s" ' (string replace -a -r -- '([\\\\"])' '\\\\\\\\$1' $arg))
     end
     vterm_printf '51;E'(string join '' $vterm_elisp)
 end
